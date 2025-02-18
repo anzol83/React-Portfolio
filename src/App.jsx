194b618cf4c64b658ac26ea0db7069794d40a738
Angel Neupane <angelneupane83@gmail.com>
2025-02-18 20:34:51 +1100

    files edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,29 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/no-unknown-property */
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-
-import DarkModeToggle from './components/DarkModeToggle'
-import Navbar from './components/NavBar'
-import HeroSection from './components/HeroSection'
-import Summary from './components/Summary'
-import SkillsSection from './components/SkillsSection'
-import RecentWorkSection from './components/RecentWorks'
-import AboutMe from './components/Aboutme'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import GoToTop from './components/GoToTop'
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage"; 
+import SkillsPage from "./components/SkillsPage";
+import DarkModeToggle from "./components/DarkModeToggle";
+import Navbar from "./components/NavBar";
+import Footer from "./components/Footer";
+import GoToTop from "./components/GoToTop";
 
 function App() {
-  // const [count, setCount] = useState(0)
   return (
     <>
-      {/* <!---DARK MODE TOGGLE----> */}
-<DarkModeToggle />
-    <header>
-      {/* <!---Navbar--> */}
-      <Navbar/>
-    </header>
-    {/* <!---HERO SECTION----> */}
-    <HeroSection/>
-    {/* <!--Summary Section---> */}
-    <Summary/>
-  {/* <!--Skills Section--> */}
-    <SkillsSection/>
-    {/* <!-- My Recent Work Section --> */}
-    <RecentWorkSection/>
-    {/* <!----About Me Section-------> */}
-    <AboutMe/>
-    {/* <!---Get In Touch Section----> */}
-    <Contact/>
-    {/* <!--Footer--> */}
-    <Footer/>
-    {/* <!-- Go to top section --> */}
-    <GoToTop/>
+      <DarkModeToggle />
+      <div className="wrapper-container">
+        <Navbar />
 
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/skills" element={<SkillsPage />} />
+        </Routes>
+
+        <Footer />
+        <GoToTop />
+      </div>
     </>
-  )
+  );
 }
-export default App
+
+export default App;
